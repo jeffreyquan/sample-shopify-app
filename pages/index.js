7fe58ab6f5ee4926964be83260e7f73ef39b2b9f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-09 23:42:54 +1000

    import resource list and store

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import store from "store-js";
+import ResourceListWithProducts from "../components/ResourceList";
+
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class Index extends React.Component {
@@ -39,13 +42,14 @@ class Index extends React.Component {
             <p>Select products to change their price temporarily</p>
           </EmptyState>
         </Layout>
+        <ResourceListWithProducts />
       </Page>
     );
   }
   handleSelection = (resources) => {
     const idsFromResources = resources.selection.map((product) => product.id);
     this.setState({ open: false });
-    console.log(idsFromResources);
+    store.set("ids", idsFromResources);
   }
 }
     
