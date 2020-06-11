f6bf128cc0ea2994eea39f8bb7592713b232b05a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-11 23:57:20 +1000

    minor formatting

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
 
 dotenv.config();
+
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
@@ -21,21 +22,21 @@ app.prepare().then(() => {
   const server = new Koa();
   server.use(session({ secure: true, sameSite: 'none' }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
+
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products', 'write_products'],
+      scopes: ["read_products", "write_products"],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
-        ctx.cookies.set(
-          'shopOrigin', shop, {
-            httpOnly: false,
-            secure: true,
-            sameSite: 'none'
-          });
-        ctx.redirect('/');
-      }
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: "none",
+        });
+        ctx.redirect("/");
+      },
     })
   );
 
