e186f4fee87c56a44b25f7e67e13dbfac8cb85aa
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-30 21:09:15 +1000

    test subscription using Billing API

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
+ const getSubscriptionUrl = require("./server/getSubscriptionUrl");
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -28,14 +30,14 @@ app.prepare().then(() => {
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: ["read_products", "write_products"],
-      afterAuth(ctx) {
+      async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set("shopOrigin", shop, {
           httpOnly: false,
           secure: true,
           sameSite: "none",
         });
-        ctx.redirect("/");
+        await getSubscriptionUrl(ctx, accessToken, shop);
       },
     })
   );
